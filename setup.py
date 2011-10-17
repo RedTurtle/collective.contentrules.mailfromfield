0ef6eebbae458db53d4d3a0dfafa6f3cc0e78cc5
keul <luca@keul.it>
2011-10-17 15:18:27 +0000

    Importing the project

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='collective.contentrules.mailfromfield',
+      version=version,
+      description="A Plone rules for send e-mail to addresses taken from a content field",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone rules mail plonegov',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='collective.contentrules.mailfromfield',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.contentrules'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.contentrules',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
