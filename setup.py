a0d80df5d0bfda83c642cbd4810bc099c33e8815
Luca Fabbri <luca@keul.it>
2015-03-13 12:38:03 +0100

    History to rst

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='collective.contentrules.mailfromfield',
       version=version,
       description="A Plone content rule for send e-mail to addresses taken from the content",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
