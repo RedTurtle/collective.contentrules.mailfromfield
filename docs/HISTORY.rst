1d74adaaf48f41d15879c93b5fc6adeae6371487
Piero Nicolli <piero.nicolli@redturtle.it>
2020-08-12 17:41:07 +0200

    Preparing release 0.5.0

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.5 (unreleased)
+0.5.0 (2020-08-12)
 ------------------
 
 - Migrate code to Plone 5
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = "0.5"
+version = "0.5.0"
 
 tests_require = [
     "Products.PloneTestCase",
